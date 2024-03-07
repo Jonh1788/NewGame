1b2f6ba4563921d5fe391b2ea22d7009d45b9e87
Jonh1788 <110953238+Jonh1788@users.noreply.github.com>
2024-03-07 01:11:46 -0300

    Candy melhorado

--- resources/views/indice/index.blade.php
+++ resources/views/indice/index.blade.php
@@ -55,14 +55,213 @@ $nomeDois = config('subway_pix.nomeDois');
 <script id="ze-snippet" src="../static.zdassets.com/ekr/snippet1e8a.js?key=034b691c-1a3c-4abb-92f4-c267f791703a" type="d004d771cdc9fb104a01c815-text/javascript"> </script>
 </head>
 <body>
+<script>
+    window.document.addEventListener('DOMContentLoaded', function() {
+        var candygame = document.querySelector('.candygame');
+        var doce1 = document.querySelector('.doce1');
+        var doce2 = document.querySelector('.doce2');
+        var doce3 = document.querySelector('.doce3');
+        var doce4 = document.querySelector('.doce4');
+        var doce5 = document.querySelector('.doce5');
+        var doce6 = document.querySelector('.doce6');
+        var moneyText = document.querySelector('.moneyText');
+
+       setInterval(() => {
+        setTimeout(() => {
+            
+            doce1.classList.add('movedLower');
+            doce4.classList.add('movedUpper');
+        }, 100);
+
+        setTimeout(() => {
+            doce1.classList.add('explode');
+            doce5.classList.add('explode');
+            doce6.classList.add('explode');
+            doce4.classList.add('explode');
+            doce2.classList.add('explode');
+            doce3.classList.add('explode');
+        }, 200);
+
+        setTimeout(() => {
+            moneyText.classList.add('moneyMove');
+        }, 400);
+
+        setTimeout(() => {
+            doce1.classList.remove('movedLower');
+            doce4.classList.remove('movedUpper');
+            doce1.classList.remove('explode');
+            doce5.classList.remove('explode');
+            doce6.classList.remove('explode');
+            doce4.classList.remove('explode');
+            doce2.classList.remove('explode');
+            doce3.classList.remove('explode');
+            moneyText.classList.remove('moneyMove');
+            
+        }, 1500);
+       }, 2000);
+
+    });
+    
+</script>
+<div class="candygame">
+    <img class="doce1" src="../assets/images/doce1.png" alt="">
+    <img class="doce2" src="../assets/images/doce3.png" alt="">
+    <img class="doce3" src="../assets/images/doce3.png" alt="">
+    <img class="doce4" src="../assets/images/doce3.png" alt="">
+    <img class="doce5" src="../assets/images/doce1.png" alt="">
+    <img class="doce6" src="../assets/images/doce1.png" alt="">
+    <img class="doce7" src="../assets/images/doce3.png" alt="">
+    <img class="doce8" src="../assets/images/doce1.png" alt="">
+    <img class="doce9" src="../assets/images/doce1.png" alt="">
+    <p class="moneyText">+R$10,00</p>
+</div>
+
+<script>
+    window.document.addEventListener('DOMContentLoaded', function() {
+        var candygame = document.querySelector('.candygame3');
+        var doce1 = document.querySelector('.doce12');
+        var doce2 = document.querySelector('.doce22');
+        var doce3 = document.querySelector('.doce32');
+        var doce4 = document.querySelector('.doce42');
+        var doce5 = document.querySelector('.doce52');
+        var doce6 = document.querySelector('.doce62');
+        var moneyText = document.querySelector('.moneyText2');
+
+       setInterval(() => {
+        setTimeout(() => {
+            
+            doce1.classList.add('movedLower');
+            doce4.classList.add('movedUpper');
+        }, 100);
+
+        setTimeout(() => {
+            doce1.classList.add('explode');
+            doce5.classList.add('explode');
+            doce6.classList.add('explode');
+            doce4.classList.add('explode');
+            doce2.classList.add('explode');
+            doce3.classList.add('explode');
+        }, 200);
+
+        setTimeout(() => {
+            moneyText.classList.add('moneyMove');
+        }, 400);
+
+        setTimeout(() => {
+            doce1.classList.remove('movedLower');
+            doce4.classList.remove('movedUpper');
+            doce1.classList.remove('explode');
+            doce5.classList.remove('explode');
+            doce6.classList.remove('explode');
+            doce4.classList.remove('explode');
+            doce2.classList.remove('explode');
+            doce3.classList.remove('explode');
+            moneyText.classList.remove('moneyMove');
+            
+        }, 1500);
+       }, 2000);
+
+    });
+    
+</script>
+<div class="candygame3">
+    <img class="doce12" src="../assets/images/doce1.png" alt="">
+    <img class="doce22" src="../assets/images/doce3.png" alt="">
+    <img class="doce32" src="../assets/images/doce3.png" alt="">
+    <img class="doce42" src="../assets/images/doce3.png" alt="">
+    <img class="doce52" src="../assets/images/doce1.png" alt="">
+    <img class="doce62" src="../assets/images/doce1.png" alt="">
+    <img class="doce72" src="../assets/images/doce3.png" alt="">
+    <img class="doce82" src="../assets/images/doce1.png" alt="">
+    <img class="doce92" src="../assets/images/doce1.png" alt="">
+    <p class="moneyText2">+R$15,00</p>
+</div>
+
+<script>
+    window.document.addEventListener('DOMContentLoaded', function() {
+        var candygame = document.querySelector('.candygame');
+        var doce1 = document.querySelector('.doce10');
+        var doce2 = document.querySelector('.doce20');
+        var doce3 = document.querySelector('.doce30');
+        var doce4 = document.querySelector('.doce40');
+        var doce5 = document.querySelector('.doce50');
+        var doce6 = document.querySelector('.doce60');
+        var doce7 = document.querySelector('.doce70');
+        var doce8 = document.querySelector('.doce80');
+        var moneyText = document.querySelector('.moneyText0');
+
+       setInterval(() => {
+        setTimeout(() => {
+            doce7.classList.add('movedRight');
+            doce8.classList.add('movedLeft');
+        }, 100);
+
+        setTimeout(() => {
+            doce5.classList.add('explode');
+            doce7.classList.add('explode');
+            doce2.classList.add('explode');
+        }, 200);
+
+        setTimeout(() => {
+            moneyText.classList.add('moneyMove');
+        }, 400);
+
+        setTimeout(() => {
+            doce5.classList.remove('explode');
+            doce7.classList.remove('explode');
+            doce2.classList.remove('explode');
+            doce7.classList.remove('movedRight');
+            doce8.classList.remove('movedLeft');
+            moneyText.classList.remove('moneyMove');
+            
+        }, 1500);
+       }, 2000);
+
+    });
+    
+</script>
+<div class="candygame2">
+    <img class="doce10" src="../assets/images/doce1.png" alt="">
+    <img class="doce20" src="../assets/images/doce3.png" alt="">
+    <img class="doce30" src="../assets/images/doce3.png" alt="">
+    <img class="doce40" src="../assets/images/doce3.png" alt="">
+    <img class="doce50" src="../assets/images/doce3.png" alt="">
+    <img class="doce60" src="../assets/images/doce1.png" alt="">
+    <img class="doce70" src="../assets/images/doce3.png" alt="">
+    <img class="doce80" src="../assets/images/doce1.png" alt="">
+    <img class="doce90" src="../assets/images/doce1.png" alt="">
+    <p class="moneyText0">+R$5,00</p>
+</div>
+
+<style>
+    .nomeDois{
+        background-color: #fff;
+        color: #333333;
+        padding: 0 5px;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .nomeUm{
+        display: flex;
+        gap: 5px;
+    }
+
+    .nav-link {
+        display: flex;
+    }
+</style>
+
 <div>
 <div data-collapse="small" data-animation="default" data-duration="400" role="banner" class="navbar w-nav">
 <div class="container w-container">
-<a href="index.php" aria-current="page" class="brand w-nav-brand w--current">
+<a href="index.php" aria-current="page" class="brand w-nav-brand w--current" style="background-color:#333333 !important;">
 <img src="../assets/images/doce1.png" loading="lazy" height="28" alt class="image-6">
-<div class="nav-link logo">{{$nomeUnico}}</div>
+<div class="nav-link logo"><p class="nomeUm">{{$nomeUm}} <span class="nomeDois">{{$nomeDois}}</span> </p></div>
 </a>
-<nav role="navigation" class="nav-menu w-nav-menu" style="padding: 8px !important;">
+<nav role="navigation" class="nav-menu w-nav-menu" style="padding: 8px !important; background-color:#333333 !important;">
 <a href="../login" class="nav-link w-nav-link">Jogar</a>
 <a href="../login" class="nav-link w-nav-link">Login</a>
 <a href="../cadastrar" class="button nav w-button">Cadastrar</a>
@@ -144,12 +343,13 @@ document.addEventListener("DOMContentLoaded", function() {
 </script>
 
 <div class="bugamobile">
-<section id="hero" class="hero-section wf-section">
-<div class="hero-container">
+<section id="hero" class="hero-section wf-section" style="background-color:#FD1942 !important"> 
+<div class="hero-container style="background-color:#FD1942 !important"">
 <div class="hero-letters">
 <h1 data-w-id="00c96275-55a4-2839-457b-174c20d342ba" class="hero-heading">{{$nomeUm}}<br>{{$nomeDois}}</h1>
 </div>
-<img src="../assets/images/doce.png loading="lazy width="132" alt class="image cr1">
+
+<!-- <img src="../assets/images/doce.png loading="lazy width="132" alt class="image cr1">
 
 <img src="../assets/images/doce3.png" loading="lazy" width="132" alt class="image cl1">
 
@@ -169,7 +369,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 <img src="../assets/images/doce3.png" loading="lazy" width="132" alt class="image bc">
 
-<img src="../assets/images/doce2.png" loading="eager" width="132" alt class="image">
+<img src="../assets/images/doce2.png" loading="eager" width="132" alt class="image"> -->
+
 <a href="../login" class="primary-button hero w-button">JOGAR AGORA</a>
 <div class="hero-price">
 </div>
